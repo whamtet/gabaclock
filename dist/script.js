ecf9cd5dd20ed3df1e97e50e0da7c28ae103b839
Matthew Molloy <whamtet@gmail.com>
2025-04-20 20:25:18 +0900

    horay!

---
+++ dist/script.js
@@ -0,0 +1,33 @@
+const x1 = 50;
+const y1 = 50;
+
+const rHour = 23;
+const rMinute = 35;
+
+const hourEl = document.querySelector("#hour");
+const minuteEl = document.querySelector("#minute");
+
+const setTime = () => {
+    const hour = Math.floor(Math.random() * 12) + 1;
+    const minute = Math.floor(Math.random() * 12) * 5;
+
+    const minuteFrac = minute / 60;
+    const hourFrac = hour / 12 + minuteFrac / 12;
+    const minuteT = Math.PI * 2 * minuteFrac;
+    const hourT = Math.PI * 2 * hourFrac;
+
+    const xMinute = x1 + rMinute * Math.sin(minuteT);
+    const yMinute = y1 - rMinute * Math.cos(minuteT);
+
+    const xHour = x1 + rHour * Math.sin(hourT);
+    const yHour = y1 - rHour * Math.cos(hourT);
+
+    minuteEl.setAttribute("x2", xMinute);
+    minuteEl.setAttribute("y2", yMinute);
+
+    hourEl.setAttribute("x2", xHour);
+    hourEl.setAttribute("y2", yHour);
+}
+
+setTime();
+document.querySelector("svg").addEventListener("click", setTime);
---
+++ gen/src/gen/core.clj
@@ -0,0 +1,23 @@
+(ns gen.core
+    (:require
+      [clojure.java.io :as io]
+      [hiccup2.core :as h]))
+
+(defn spit-template [& args]
+  (spit
+    "../dist/index.html"
+   (format
+    (-> "index.html" io/resource slurp)
+    (str
+     (h/html
+       args)))))
+
+(def r 41)
+(spit-template
+ (for [i (range 1 13)]
+   (let [t (* 2 Math/PI i 1/12)
+         x (+ 48.25 (* r (Math/sin t)))
+         y (+ 52 (* -1 r (Math/cos t)))
+         textLength (if (< i 10) 0.1 0.3)]
+     [:text {:x x :y y :fill "black" :font-size "5"} i])))
+
